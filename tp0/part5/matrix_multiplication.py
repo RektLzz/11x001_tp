6ce74cbf866ec8fcf6cceb1bf187fea5afccb470
Matthew <matthewodonnell285@gmail.com>
2024-09-19 21:52:41 +0200

    Are you Stand proud, You're Strong because your Nah I'd Win, or are you Nah I'd Win because you're Stand Proud, You're Strong?

--- tp0/part5/matrix_multiplication.py
+++ tp0/part5/matrix_multiplication.py
@@ -1,19 +1,31 @@
 import numpy as np
 
+def MatrixProduct(A, B):
+    
+    A_rows = len(A)
+    A_cols = len(A[0])
+
+    B_rows = len(B)
+    B_cols = len(B[0]) 
+
+    C = [[0 for _ in range(B_cols)] for _ in range(A_rows)]
+
+    # Check if the matrices can be added (same shape)
+    if A_cols != B_rows:
+        raise ValueError("Matrices must have the same dimensions for addition.")
+    
+    for i in range(A_rows):
+        for j in range(B_cols):
+            for k in range(A_cols):
+                C[i][j] += A[i][k] * B[k][j]
+
+    return C
+
+
 A = [[1, 2, 3, 4]]  # 1 x 4
 B = [[1], [2], [3], [4]]    # 4 x 1
 
-C = [[0]]
+matrix_a = [[1, 2], [3, 4]]
+matrix_b = [[5, 6], [7, 8]]
 
-for i in range(1):
-    for j in range(1):
-        for k in range(4):
-            C[i][j] = C[i][j] + A[i][k]*B[k][j]
-
-print(C)
-
-A_np = np.array(A)
-B_np = np.array(B)
-C_np = A_np @ B_np
-
-print(C_np)
+print(MatrixProduct(matrix_a, matrix_b))
