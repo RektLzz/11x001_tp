3d6ed5fe63341244f87c7cfa06d73171cc338d2e
Matthew <matthewodonnell285@gmail.com>
2024-10-12 16:45:16 +0200

    Finished TP3

--- tp3/main_empty.c
+++ tp3/main_empty.c
@@ -7,26 +7,81 @@
 
 
 // EXERCICE 1
-
+float somme(float x, float y) {
+    return x + y;
+}
 
 // EXERCICE 2
+int puissance(int base, int exposant) {
+    
+    float resultat = 1;
+    for (int i = 0; i < exposant; i++) {
+        resultat *= base;
+    }
 
+    return resultat;
+}
 
 // EXERCICE 4
 
 int restreindre_intervalle_pure(int x, int min, int max) {
     /******************** Votre code ci-dessous ********************/
+    if (x < min) {
+        return min;
+    }
+    else if (x > max) {
+        return max;
+    }
+    else {
+        return x;
+    }
+
     /******************** Votre code ci-dessus ********************/
 }
 
 void restreindre_intervalle_bord(int *x, int min, int max) {
     /******************** Votre code ci-dessous ********************/
+    if (*x < min) {
+        *x = min;
+    }
+    else if (*x > max){
+        *x = max;
+    }
+
     /******************** Votre code ci-dessus ********************/
 }
 
 // EXERCICE 5
+void echange(int* x, int* y) {
+    
+    int c = *y;
+    
+    *y = *x;
+    *x = c;
+}
 
 // EXERCICE 6
+int factorielle(int n){
+    if (n <= 0){
+        return 1;
+    }
+
+    return n * factorielle(n-1);
+}
+
+int coefficient_binomial(int k, int n) {
+    return (factorielle(n) / (factorielle(k) * factorielle(n-k)));
+}
+
+int factorielle_iteratif(int n){
+    int resultat = 1;
+
+    for (int i=1; i <= n; i++) {
+        resultat *= i;            
+    }
+
+    return resultat;
+}
 
 // EXERCICE 7
 
@@ -65,19 +120,71 @@ void hanoi(int nb_disques, char nom_tour_depart, char nom_tour_arrivee, char nom
     }
 
     // TODO : commenter (ou supprimer) la ligne suivante
-    hanoi_idiot(nb_disques, nom_tour_depart, nom_tour_arrivee, nom_tour_auxiliaire);
+    //hanoi_idiot(nb_disques, nom_tour_depart, nom_tour_arrivee, nom_tour_auxiliaire);
 
     /******************** Votre code ci-dessous ********************/
-    
-    
+    if (nb_disques == 1) {
+        afficher_instruction_hanoi(nom_tour_depart, nom_tour_arrivee);
+    }
+    else {
+        hanoi(nb_disques - 1, nom_tour_depart, nom_tour_auxiliaire, nom_tour_arrivee);
+        afficher_instruction_hanoi(nom_tour_depart, nom_tour_arrivee);
+        hanoi(nb_disques -1, nom_tour_auxiliaire, nom_tour_arrivee, nom_tour_depart);
+    }
     /******************** Votre code ci-dessus *********************/
 }
 
 // EXERCICE 8
+float valeur_absolue(float a) {
+    if (a < 0) {
+        return -a;
+    }
+    else {
+        return a;
+    }
+}
 
+float initialiser_heron(float a) {
+    float x = 0;
+    while (x*x <= a) {
+        x++;
+    }
+
+    return x - 1;
+}
+
+float racine_heron(float a, float precision) {
+    float x = initialiser_heron(a);
+
+    while (valeur_absolue(x*x - a) > precision) {
+        x = (x + (a/x)) / 2;
+    }
+
+    return x;
+}
 
 // EXERCICE 9 
+float approximer_pi(int nombre_points_par_axes)  {
 
+    float dx = 1 / (float) nombre_points_par_axes; // Length / #points = seperation per point
+    float dy = 1 / (float) nombre_points_par_axes;
+
+    int points_dans_cercles = 0;
+    int points_en_tout = 0;
+
+    for (float x = 0.0; x <= 1.0; x+=dx) {
+        for (float y = 0.0; y <= 1.0; y+=dy) {
+
+            if ((x-0.5)*(x-0.5) + (y-0.5)*(y-0.5) <= 0.25) {
+                points_dans_cercles++;
+            }
+
+            points_en_tout++;
+        }
+    }
+
+    return 4 * (float) points_dans_cercles / (float) points_en_tout;
+}
 
 /****************************/
 /* Vos fonctions ci-dessus **/
@@ -93,7 +200,7 @@ void exercice1(void) {
     scanf("%f %f", &a, &b);
     
     // TODO : Décommenter la ligne suivante une fois que la fonction somme est implémentée
-    // c = somme(a, b); 
+    c = somme(a, b); 
     
     printf("%f + %f = %f\n", a, b, c);
 
@@ -110,7 +217,7 @@ void exercice2(void) {
     scanf("%d %d", &nombre, &exposant); 
 
     /******************** Votre code ci-dessous ********************/
-   
+    resultat = puissance(nombre, exposant);
     /******************** Votre code ci-dessus *********************/
 
     printf("%d^%d = %d\n", nombre, exposant, resultat);
@@ -126,7 +233,18 @@ void exercice3(void) {
     printf("a = %d (adresse : %p) \n", a, &a);
     
     /******************** Votre code ci-dessous ********************/
-    
+    int* p1;
+    int* p2;
+
+    printf("a = %d (adresse : %p) \n", a, &a);
+    p1 = &a;
+    printf("a = %d (adresse : %p) \n", a, &a);
+    *p1 = *p1 - 3;
+    printf("a = %d (adresse : %p) \n", a, &a);
+    p2 = p1;
+    printf("a = %d (adresse : %p) \n", a, &a);
+    *p2 = *p2 * 6;
+    printf("a = %d (adresse : %p) \n", a, &a);
     /******************** Votre code ci-dessus *********************/
 
     return;
@@ -166,7 +284,7 @@ void exercice5(void) {
     printf("Avant l'échange, a = %d et b = %d \n", a, b);
     
     /******************** Votre code ci-dessous ********************/
-
+    echange(&a,&b);
     /******************** Votre code ci-dessus *********************/
 
     printf("Après l'échange, a = %d et b = %d \n", a, b);
@@ -187,7 +305,11 @@ void exercice6(void) {
     scanf("%d", &n);
 
     /******************** Votre code ci-dessous ********************/
-    
+    fact_k = factorielle(k);
+    fact_n = factorielle(n);
+
+    k_parmi_n = coefficient_binomial(k, n);
+
     /******************** Votre code ci-dessus *********************/
 
     printf("%d! = %d\n", k, fact_k);
@@ -226,7 +348,7 @@ void exercice8(void) {
     scanf("%f", &precision);
     
     // TODO : Décommenter la ligne suivante une fois que la fonction ,'racine_heron' est implémentée
-    // x = racine_heron(a, precision);
+    x = racine_heron(a, precision);
 
     printf("Racine carrée de %f = %f\n", a, x);
 
@@ -244,7 +366,7 @@ void exercice9(void) {
     scanf("%d", &nb_points_par_axe);
 
     /******************** Votre code ci-dessous ********************/
-        
+    printf("Pi = %f\n", approximer_pi(nb_points_par_axe));
     /******************** Votre code ci-dessus *********************/
 
     return;
@@ -255,14 +377,14 @@ int main(void) {
 
     // Astuce : commenter tous les exercices sauf celui en cours pour gagner du temps !
 
-    exercice1();
-    exercice2();
-    exercice3();
-    exercice4();
-    exercice5();
-    exercice6();
-    exercice7();
-    exercice8();
+    //exercice1();
+    //exercice2();
+    //exercice3();
+    //exercice4();
+    //exercice5();
+    //exercice6();
+    //exercice7();
+    //exercice8();
     exercice9();
     
     return 0;
